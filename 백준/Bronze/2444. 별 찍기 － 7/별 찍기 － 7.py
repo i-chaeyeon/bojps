21a76c0fdbca582c869712e0a16f09c89b1f210c
i-chaeyeon <143814340+i-chaeyeon@users.noreply.github.com>
2024-02-21 16:21:35 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -1,20 +1,5 @@
-N = int(input())
-
-blank = N-1
-for i in range (N):
-    for _ in range (blank):
-        print(" ", end="")
-    for s in range (2*i+1):
-        print("*", end="")
-    print()
-    blank -=1
-
-blank = 1
-while(blank <= N-1):
-    for _ in range (blank):
-        print(" ", end="")
-        iter = 2*(N-blank)-1
-    for s in range (iter):
-        print("*", end="")
-    print()
-    blank += 1
+n = int(input())
+for i in range(1, n):
+    print(' '*(n-i) + '*'*(2*i-1))
+for i in range(n, 0, -1):
+    print(' '*(n-i) + '*'*(2*i-1))
