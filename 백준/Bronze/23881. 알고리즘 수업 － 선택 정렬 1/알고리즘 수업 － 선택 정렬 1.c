dc048528fa26cf07066550a7aa2134da52b33310
i-chaeyeon <143814340+i-chaeyeon@users.noreply.github.com>
2024-04-08 16:50:43 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 160 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+    int N, K;
+    scanf("%d %d", &N, &K);
+
+    int *arr; 
+    arr = (int*) malloc(sizeof(int)*N);
+
+    for (int i=0; i<N; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    int cnt=0;
+
+    int temp = 0;
+    int max = 0;
+    for (int i=N-1; i>=0; i--){
+        int maxIdx = i;
+        for (int j=0; j<i; j++){
+            if (arr[j] > arr[maxIdx]){
+                maxIdx = j;
+            }
+        }
+
+        if (maxIdx != i){
+            temp = arr[i];
+            arr[i] = arr[maxIdx];
+            max = arr[maxIdx];
+            arr[maxIdx] = temp;
+            cnt++;
+        }
+
+        if (cnt == K)
+            break;
+    }
+
+    if (cnt < K){
+        printf("-1 \n");
+    } else {
+        printf("%d %d \n", temp,max);
+    }
+
+    free(arr);
+    return 0;
+}
