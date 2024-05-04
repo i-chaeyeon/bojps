0ab3de8e4ee74e0309fbcf890c7f16df113f55b4
i-chaeyeon <143814340+i-chaeyeon@users.noreply.github.com>
2024-05-04 23:58:02 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+
+    int sum = 0;
+
+    for(int i=0; i<5; i++){
+        int num = 0;
+        scanf("%d", &num);
+
+        sum += num*num;
+    }
+
+    printf("%d", sum%10);
+
+    return 0;
+}
