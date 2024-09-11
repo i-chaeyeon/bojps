8e8b4092230837811397213437a9b861e4d08a16
i-chaeyeon <143814340+i-chaeyeon@users.noreply.github.com>
2024-09-11 09:37:05 +0900

    [Bronze IV] Title: 강당 대관, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/31994. 강당 대관/강당 대관.py
@@ -0,0 +1,12 @@
+ans = ''
+curMax = 0
+
+for _ in range (7):
+    seminar, num = map(str, input().split())
+    
+    if int(num) > curMax :
+        ans = seminar
+        curMax = int(num)
+    
+
+print(ans)
